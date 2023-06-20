06ac8b630e64fa15b4cf85a39326348b2c4ebcdd
Maryam <maryamjaved.xo@gmail.com>
2023-06-20 17:47:39 +0500

    call stack

---
+++ main.js
@@ -0,0 +1,19 @@
+// ---------call stack
+
+function first(){
+    console.log('first');
+    second();
+}
+
+function second(){
+    console.log('second');
+    third();
+}
+
+function third(){
+    console.log('third');
+}
+
+first();
+
+
