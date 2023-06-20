266a1f6f70b5e695de72a06ba46f7f61293c07a1
Maryam <maryamjaved.xo@gmail.com>
2023-06-20 17:51:09 +0500

    execution context

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 // ---------call stack
 
-function first(){
-    console.log('first');
-    second();
+// function first(){
+//     console.log('first');
+//     second();
+// }
+
+// function second(){
+//     console.log('second');
+//     third();
+// }
+
+// function third(){
+//     console.log('third');
+// }
+
+// first();
+
+
+
+//-------------execution context
+
+var x=100;
+var y=50;
+
+function getSum(n1, n2){
+    var sum=n1+n2;
+    return sum;
 }
 
-function second(){
-    console.log('second');
-    third();
-}
-
-function third(){
-    console.log('third');
-}
-
-first();
-
-
+var sum1=getSum(x, y);
+var sum2=getSum(100,50);
